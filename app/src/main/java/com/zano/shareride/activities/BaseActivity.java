efe604fec5b065d14fe6f22599c711ed7291c4d0
Zano-BeastPC\Zano <andreazanottifo@gmail.com>
2017-06-06 19:03:02 +0200

    Avanzamento scheletro

--- app/src/main/java/com/zano/shareride/activities/BaseActivity.java
+++ app/src/main/java/com/zano/shareride/activities/BaseActivity.java
@@ -1,8 +1,14 @@
 package com.zano.shareride.activities;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 
+import com.zano.shareride.R;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
@@ -11,15 +17,43 @@ import butterknife.ButterKnife;
 
 public abstract class BaseActivity extends AppCompatActivity {
 
+    private static final String TAG = "BaseActivity";
+
+    @BindView(R.id.my_toolbar) protected Toolbar toolbar;
+
+    protected ProgressDialog progressDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(layoutId());
         ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+    }
+
+
+    protected void showProgressDialog(String message) {
+        if(progressDialog == null) {
+            progressDialog = new ProgressDialog(BaseActivity.this);
+            progressDialog.setIndeterminate(true);
+            progressDialog.setMessage(message);
+            progressDialog.show();
+        } else {
+            Log.d(TAG, "onShowProgressDialog:dialog already shown");
+        }
+    }
+
+    public void closeProgressDialog() {
+        if(progressDialog != null) {
+            progressDialog.dismiss();
+        } else {
+            Log.d(TAG, "onCloseProgressDialog:dialog not active");
+        }
     }
 
     /**
      * @return the id referencing the layout to use for this activity
      */
     protected abstract int layoutId();
+
 }
---
+++ app/src/main/java/com/zano/shareride/constants/Constants.java
@@ -0,0 +1,15 @@
+package com.zano.shareride.constants;
+
+import android.support.annotation.StringDef;
+
+/**
+ * Created by Zano on 06/06/2017, 18:30.
+ */
+
+public class Constants {
+
+    @StringDef({ParcelArgs.NAMECLASS})
+    public @interface ParcelArgs {
+        String NAMECLASS = "NAMECLASS";
+    }
+}
